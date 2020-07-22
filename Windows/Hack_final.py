08fca683ba9edb6288d60732329b3d3e86f07d2a
unknown <shivankkapoor5@gmail.com>
2020-07-22 08:12:22 +0530

    Windows LogicBomb Bundling

---
+++ Windows/Hack_final.py
@@ -0,0 +1,11 @@
+def main():
+  import webbrowser
+  i=0
+  
+  while(i<10):
+     webbrowser.open("http://abcefghijk.com")
+     i=i+1
+    
+
+
+
---
+++ Windows/cli.py
@@ -0,0 +1,4 @@
+from Hack_final  import main
+
+if __name__== '__main__':
+   main() 
